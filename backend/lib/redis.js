cd2e96ee39b9e6dcdf27501b047a6e5e4f528b82
abdullah <hk7485966@gmail.com>
2026-06-29 20:01:59 +0500

    updated docker file,add docker ignore

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -1,6 +1,16 @@
+// import Redis from "ioredis";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
+
+
 import Redis from "ioredis";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+console.log("Redis URL:", process.env.UPSTASH_REDIS_URL);
 
 export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
